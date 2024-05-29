4bae9b9fa47ce8ac0d2f8c94ec4346b93c7e2de0
am-User334 <mvoda98@gmail.com>
2024-05-29 13:03:34 +0200

    Itemdrops added

--- src/main/java/at/mtxframe/mtxframe/gui/Tablist.java
+++ src/main/java/at/mtxframe/mtxframe/gui/Tablist.java
@@ -1,6 +1,7 @@
 package at.mtxframe.mtxframe.gui;
 
 import at.mtxframe.mtxframe.MtxFrame;
+import at.mtxframe.mtxframe.colors.format.ColorFormat;
 import at.mtxframe.mtxframe.database.DatabasePlayerStats;
 import at.mtxframe.mtxframe.models.PlayerStatsModel;
 import org.bukkit.ChatColor;
@@ -9,17 +10,19 @@ import org.bukkit.entity.Player;
 import java.sql.SQLException;
 
 public class Tablist {
-    DatabasePlayerStats database = new DatabasePlayerStats(MtxFrame.getPlugin());
+    MtxFrame plugin = MtxFrame.getPlugin();
+    ColorFormat colorFormat = new ColorFormat(plugin.getColorAPI());
+    DatabasePlayerStats database = new DatabasePlayerStats(plugin);
 
     public void setTabList(Player player){
-        player.setPlayerListHeader(ChatColor.AQUA + "EridiumRPG.net");
+        player.setPlayerListHeader(colorFormat.formatText("&XEridiumRPG"));
         try {
             PlayerStatsModel stats = database.findPlayerStatDataByUUID(String.valueOf(player.getUniqueId()));
             if (!stats.getPlayerGuild().equals("No Guild")) {
                 player.setPlayerListName(ChatColor.GOLD + "[GILDE: " + stats.getPlayerGuild() + "] " + ChatColor.AQUA + " | " + ChatColor.WHITE + player.getDisplayName());
             }
             else if (!player.isOp()){
-                player.setPlayerListName(ChatColor.GREEN + "[SOLO SPIELER] " + ChatColor.AQUA + " | " + ChatColor.WHITE + player.getName());
+                player.setPlayerListName(ChatColor.GREEN + "[SPIELER] " + ChatColor.AQUA + " | " + ChatColor.WHITE + player.getName());
             } else if (player.isOp()) {
                 player.setPlayerListName(ChatColor.DARK_PURPLE + "[Server Team] " + ChatColor.AQUA + " | " + ChatColor.WHITE + player.getName());
             }
